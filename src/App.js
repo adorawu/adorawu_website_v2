b1a638aab21d8e8146627a4b6d857ef1f1c685b3
adorawu <wu.adora@gmail.com>
2023-08-24 23:54:50 -0500

    First Commit

---
+++ src/App.js
@@ -0,0 +1,56 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div id="Page">
+        <div id="Background"></div>
+        <div class="mask" id="Mask">
+          <div class="mask_top"></div>
+          <div class="mask_bottom"></div>
+        </div>
+
+        <div class="frame" id="Frame">
+          <div class="frame_line frame_line-left"></div>
+          <div class="frame_line frame_line-right"></div>
+          <div class="frame_line frame_line-top"></div>
+          <div class="frame_line frame_line-bottom"></div>
+        </div>
+
+        <nav class="navbar" id="mainNav">
+            <a class="nav_left" href="#landing">Home</a>
+            <div class="nav_right">
+              <a class="nav_link" href="#about">About</a>
+              <a class="nav_link" href="#experience">Experience</a>
+              <a class="nav_link" href="#projects">Projects</a>
+              <a class="nav_link" href="#contact">Contact</a>
+            </div>
+        </nav>
+
+        <div class="landing_page">
+          <h1 class="landing_page_text">hi. my name is</h1>
+          <br></br>
+          <h1 class="landing_page_name">Adora Wu</h1>
+          <br></br>
+          <h1 class="landing_page_text">Aspiring Software Engineer</h1>
+        </div>
+
+        <div class="about page" id="about">
+            <div class="about-text">
+                <h2 class="fancy-text">about me</h2>
+                <p class="about-me-text">I'm a student currently attending the 
+                    University of Texas at Austin pursuing chemistry and computer science. I'm passionate about technology, natural sciences, and design. In my free time I enjoy digital art, video games, and trying new recipies! 
+                </p>
+            </div>
+            <div class="about-pic">
+                <img class="headshot" src="headshot.jpg" alt="A picture of me"></img>
+            </div>
+        </div>
+
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
